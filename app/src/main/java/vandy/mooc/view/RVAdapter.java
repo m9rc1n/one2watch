44b7fa3c446bc323bf723e0c31546e07e238f283
marc-inn <0.660162@gmail.com>
2016-01-25 23:39:53 +0100

    Work with video

--- app/src/main/java/vandy/mooc/view/RVAdapter.java
+++ app/src/main/java/vandy/mooc/view/RVAdapter.java
@@ -1,7 +1,9 @@
 package vandy.mooc.view;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -26,48 +29,70 @@ import vandy.mooc.model.aidl.TrailerData;
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
 
     private final ImageLoader mImageLoader;
+    private final Resources mRes;
+    private final Context mContext;
     ArrayList<TrailerData> mTrailers;
 
     public RVAdapter(ArrayList<TrailerData> trailers, Context context) {
         mTrailers = trailers;
         mImageLoader = ImageLoader.getInstance();
         mImageLoader.init(ImageLoaderConfiguration.createDefault(context));
+        mRes = context.getResources();
+        mContext = context;
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(PersonViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
     }
 
     @Override
     public void onBindViewHolder(final PersonViewHolder personViewHolder, int i) {
         final TrailerData data = mTrailers.get(i);
-        mImageLoader.displayImage(data.getThumb().getSmall(),
-                personViewHolder.personPhoto,
+        mImageLoader.loadImage(data.getThumb().getSmall(),
                 DisplayImageOptions.createSimple(),
                 new ImageLoadingListener() {
                     @Override
                     public void onLoadingStarted(String imageUri, View view) {
-
                     }
 
                     @Override
                     public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-
                     }
 
                     @Override
                     public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-
+                        //                        personViewHolder.video.setBackground(new BitmapDrawable(mRes, loadedImage));
                     }
 
                     @Override
                     public void onLoadingCancelled(String imageUri, View view) {
-
                     }
                 });
         personViewHolder.personName.setText(data.getMovie().getTitle());
         personViewHolder.personAge.setText(data.getMovie().getPlot());
-        personViewHolder.cv.setOnClickListener(new View.OnClickListener() {
+        personViewHolder.video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                personViewHolder.play.setVisibility(View.VISIBLE);
+            }
+        });
+        personViewHolder.video.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                personViewHolder.play.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
+        personViewHolder.play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 personViewHolder.video.setVideoURI(Uri.parse(data.getEmbed().getHtml5().get360p()));
-                personViewHolder.video.start();
+                MediaController controller = new MediaController(mContext);
+                controller.setAnchorView(personViewHolder.video);
+                personViewHolder.video.setMediaController(controller);
+                personViewHolder.video.requestFocus();
+                v.setVisibility(View.GONE);
             }
         });
     }
@@ -94,15 +119,21 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
         CardView cv;
         TextView personName;
         TextView personAge;
-        ImageView personPhoto;
+        ImageView play;
 
         PersonViewHolder(View itemView) {
             super(itemView);
             cv = (CardView) itemView.findViewById(R.id.cv);
             personName = (TextView) itemView.findViewById(R.id.person_name);
             personAge = (TextView) itemView.findViewById(R.id.person_age);
-            personPhoto = (ImageView) itemView.findViewById(R.id.person_photo);
             video = (VideoView) itemView.findViewById(R.id.videoView);
+            play = (ImageView) itemView.findViewById(R.id.imageView);
+            video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    video.start();
+                }
+            });
         }
     }
 
