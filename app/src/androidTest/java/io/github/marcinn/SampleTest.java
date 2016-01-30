d8508acb6d2863522d83c0f29fcdd851cc48535a
marc-inn <0.660162@gmail.com>
2016-01-30 12:38:11 +0100

    Add simple json parsing test and update gradle

--- app/src/androidTest/java/io/github/marcinn/SampleTest.java
+++ app/src/androidTest/java/io/github/marcinn/SampleTest.java
@@ -8,7 +8,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import io.github.marcinn.model.aidl.TrailerData;
+import io.github.marcinn.model.aidl.TrailerDataJsonParser;
 import io.github.marcinn.view.MainActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -18,7 +27,16 @@ public class SampleTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void sample_sameActivity() {
+    public void shouldTrailerJsonParserReturnCorrectStructure() {
+        InputStream is = getInstrumentation().getTargetContext()
+                .getResources()
+                .openRawResource(R.raw.popular_trailers);
+        final TrailerDataJsonParser parser = new TrailerDataJsonParser();
+        try {
+            List<TrailerData> returnList = parser.parseJsonStream(is);
+            assertTrue(returnList.size() == 10);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
