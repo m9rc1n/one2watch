465f8c8e41d6ad7eac50f2b828b53434e02e9ba7
marc-inn <0.660162@gmail.com>
2016-02-01 20:20:12 +0100

    Integrate local unit test with travis

--- app/src/test/java/io/github/marcinn/SampleTest.java
+++ app/src/test/java/io/github/marcinn/SampleTest.java
@@ -2,6 +2,8 @@ package io.github.marcinn;
 
 import android.test.suitebuilder.annotation.LargeTest;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 @LargeTest
@@ -9,5 +11,6 @@ public class SampleTest {
 
     @Test
     public void testSample() {
+        Assert.assertEquals(1, 1);
     }
 }
