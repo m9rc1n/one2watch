01fb9dd3aaf5477884e9cd5e2f7af3415ffeccd2
marc-inn <0.660162@gmail.com>
2016-02-01 19:48:17 +0100

    Add empty unit test (previous one were instrumental)

---
+++ app/src/test/java/io/github/marcinn/SampleTest.java
@@ -0,0 +1,13 @@
+package io.github.marcinn;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Test;
+
+@LargeTest
+public class SampleTest {
+
+    @Test
+    public void testSample() {
+    }
+}
