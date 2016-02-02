72ea376c399145a362417879b2de8957a46f5363
marc-inn <0.660162@gmail.com>
2016-02-02 15:40:51 +0100

    Success while reading a test resources file!

--- app/src/test/java/io/github/marcinn/SampleTest.java
+++ app/src/test/java/io/github/marcinn/SampleTest.java
@@ -6,11 +6,14 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import java.net.URL;
+
 @LargeTest
 public class SampleTest {
 
     @Test
     public void testSample() {
         Assert.assertEquals(1, 1);
+        URL url = this.getClass().getClassLoader().getResource("popular_trailers.json");
     }
 }
