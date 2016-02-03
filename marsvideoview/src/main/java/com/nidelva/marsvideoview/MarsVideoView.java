d1540b9f916c7403fa5b4ce111ab9cfb9be66dc1
marc-inn <0.660162@gmail.com>
2016-02-03 14:52:53 +0100

    Pause playback when new video starts

--- marsvideoview/src/main/java/com/nidelva/marsvideoview/MarsVideoView.java
+++ marsvideoview/src/main/java/com/nidelva/marsvideoview/MarsVideoView.java
@@ -9,40 +9,55 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.VideoView;
 
+import java.util.UUID;
+
 public class MarsVideoView extends VideoView {
 
     public static final String ACTION_STOP = "ACTION_STOP";
+    public static final String EXTRA_UUID = "EXTRA_UUID";
+    private UUID mUUID;
 
     public MarsVideoView(Context context) {
         super(context);
-        context.registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (ACTION_STOP.equals(intent.getAction())) {
-                    if (isPlaying()) {
-                        stopPlayback();
-                    }
-                }
-            }
-        }, new IntentFilter(ACTION_STOP));
+        registerReceiver(context);
     }
 
     public MarsVideoView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        registerReceiver(context);
     }
 
     public MarsVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        registerReceiver(context);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public MarsVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        registerReceiver(context);
+    }
+
+    private void registerReceiver(Context context) {
+        context.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (ACTION_STOP.equals(intent.getAction())) {
+                    if (intent.getStringExtra(EXTRA_UUID).equals(mUUID.toString())) return;
+                    if (isPlaying()) {
+                        pause();
+                    }
+                }
+            }
+        }, new IntentFilter(ACTION_STOP));
+        mUUID = UUID.randomUUID();
     }
 
     @Override
     public void start() {
         super.start();
-        getContext().sendBroadcast(new Intent(ACTION_STOP));
+        Intent intent = new Intent(ACTION_STOP);
+        intent.putExtra(EXTRA_UUID, mUUID.toString());
+        getContext().sendBroadcast(intent);
     }
 }
